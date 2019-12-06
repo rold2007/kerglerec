909e12785f46d8472b958f0003ebd6d7d181e5af
David Rolland <rold2007@yahoo.com>
2019-12-06 21:21:21 +0900

    Added the World class.

---
+++ Src/Kerglerec/World.cs
@@ -0,0 +1,71 @@
+﻿// <copyright file="World.cs" company="David Rolland">
+// Copyright (c) David Rolland. All rights reserved.
+// </copyright>
+
+namespace Kerglerec
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Text;
+
+   /// <summary>
+   /// Class which represent the whole world.
+   /// </summary>
+   public class World
+   {
+      private HashSet<Province> provinces;
+      private Calendar calendar;
+
+      /// <summary>
+      /// Initializes a new instance of the <see cref="World"/> class.
+      /// </summary>
+      public World()
+      {
+         this.provinces = new HashSet<Province>();
+         this.calendar = new Calendar();
+      }
+
+      /// <summary>
+      /// Gets all the provinces in the world.
+      /// </summary>
+      public IReadOnlyCollection<Province> Provinces
+      {
+         get
+         {
+            return this.provinces;
+         }
+      }
+
+      /// <summary>
+      /// Add a province to the world.
+      /// </summary>
+      /// <param name="province">Province to add.</param>
+      public void Add(Province province)
+      {
+         this.provinces.Add(province);
+      }
+
+      /// <summary>
+      /// Advance world time one step.
+      /// </summary>
+      public void Tick()
+      {
+         this.calendar.Add(1);
+
+         foreach (Province province in this.provinces)
+         {
+            Harvest harvest = new Harvest();
+
+            Food foodProduction = harvest.FoodProduction(this.calendar, province);
+
+            province.Add(foodProduction);
+
+            BirthControl birthControl = new BirthControl();
+
+            Population populationFlow = birthControl.PopulationFlow(this.calendar, province);
+
+            province.Add(populationFlow);
+         }
+      }
+   }
+}
---
+++ Tests/Kerglerec.Tests/WorldTests.cs
@@ -0,0 +1,72 @@
+﻿// <copyright file="WorldTests.cs" company="David Rolland">
+// Copyright (c) David Rolland. All rights reserved.
+// </copyright>
+
+namespace Kerglerec.Tests
+{
+   using System;
+   using System.Collections.Generic;
+   using System.Text;
+   using Shouldly;
+   using Xunit;
+
+   /// <summary>
+   /// Contains tests for the World class.
+   /// </summary>
+   public class WorldTests
+   {
+      /// <summary>
+      /// Tests the World constructor.
+      /// </summary>
+      [Fact]
+      public void ConstructorTest()
+      {
+         World world = new World();
+      }
+
+      /// <summary>
+      /// Tests the World::Add method.
+      /// </summary>
+      [Fact]
+      public void AddTest()
+      {
+         World world = new World();
+         Province province = new Province();
+
+         world.Provinces.Count.ShouldBe(0);
+
+         world.Add(province);
+
+         world.Provinces.Count.ShouldBe(1);
+         world.Provinces.ShouldContain(province);
+      }
+
+      /// <summary>
+      /// Tests the World::Tick method.
+      /// </summary>
+      [Fact]
+      public void TickTest()
+      {
+         World world = new World();
+         Province province = new Province();
+         Population population = new Population();
+         Food food = new Food();
+
+         population.Add(1000);
+         food.Add(12000);
+
+         province.Add(population);
+         province.Add(food);
+
+         world.Add(province);
+
+         for (int month = 0; month < 12; month++)
+         {
+            world.Tick();
+         }
+
+         province.Population.Adult.ShouldBeGreaterThan(population.Adult);
+         province.Food.Rice.ShouldBeGreaterThan(food.Rice);
+      }
+   }
+}
